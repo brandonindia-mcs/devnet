3c46708d433d92cde7833eb48b33f506efac320e
Brandon R. India <brandonindia-mcs>
2025-09-13 13:42:59 -0500

    networking to cluster

--- kubernetes.docker/k8s-lab-00/lab00.sh
+++ kubernetes.docker/k8s-lab-00/lab00.sh
@@ -1,7 +1,10 @@
 #!/usr/local/bin/bash
 . ./k.config.sh
+kubectl config use-context docker-desktop
 _project=k8s-lab-00
 lab_cluster="${1:-lab-cluster}"
+containerPort="${containerPort:-30080}"
+hostPort="${hostPort:-30080}"
 export _cluster=$lab_cluster && kind=kind-
 kube_context
 
@@ -11,9 +14,16 @@ kubernetes_version=$(kubectl version|grep -i 'client version'|awk '{print $3}')\
 echo|cat >./$_project/$_cluster-config.yaml <<EOF
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
+networking:
+  serviceSubnet: "10.97.0.0/16"  # Avoids overlap with docker-desktop (10.96.0.0/12)
+  podSubnet: "10.98.0.0/16"
 nodes:
 - role: control-plane
   image: kindest/node:$kubernetes_version
+  extraPortMappings:
+    - containerPort: $containerPort  # Maps NodePort to host for ingress
+      hostPort: $hostPort
+      protocol: TCP
 - role: worker
   image: kindest/node:$kubernetes_version
 - role: worker
@@ -23,3 +33,6 @@ EOF
 # NEW CLUSTER
 kind create cluster -n $_cluster --config ./$_project/$_cluster-config.yaml\
   || kubectl config use-context ${kind}$_cluster
+kube_context
+
+echo $_project script finished
